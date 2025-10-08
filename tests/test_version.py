e5252da66c9323be0ae3d2254c75c361982d9dab
Peter Javorsky <tekk.sk@gmail.com>
2025-10-08 23:05:32 +0200

    Fix version test to use correct package name 'mt-downloader'

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,5 +5,5 @@ def test_package_version_matches_dunder():
     # Ensure installed distribution version matches module __version__
     import map_tiles_downloader as mtd
 
-    dist_ver = version("map-tiles-downloader")
+    dist_ver = version("mt-downloader")
     assert mtd.__version__ == dist_ver
